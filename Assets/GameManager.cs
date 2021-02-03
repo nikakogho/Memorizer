5e12a869518472787c29d6bf29faad59a61fa6ba
nikakogho <nikakoghuashvili@gmail.com>
2021-02-03 15:44:30 +0400

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public string[] allWords;
+    public int amount;
+    public GameObject gamePlayUI;
+    public GameObject entranceUI;
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    [ContextMenu("Test Extra Words")]
+    void TestExtraWords()
+    {
+        bool allClear = true;
+
+        for (int i = 0; i < allWords.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (allWords[i] == allWords[j])
+                {
+                    Debug.LogError("Elements " + i + " and " + j + " have the same value!");
+                    allClear = false;
+                }
+            }
+        }
+
+        if (allClear)
+            Debug.Log("All Clear!");
+    }
+
+    public void StartGame()
+    {
+        entranceUI.SetActive(false);
+        gamePlayUI.SetActive(true);
+    }
+
+    public void OnWordAmountChanged(string value)
+    {
+        amount = int.Parse(value);
+    }
+
+    public void Again()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/GamePlayManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GamePlayManager : MonoBehaviour {
+    string[] words;
+    int index = -1;
+
+    public GameObject showUI;
+    public GameObject guessUI;
+    public GameObject congratulationsUI;
+    public Button confirmGuessButton;
+    public Text showText;
+    public InputField guessField;
+    public Text guessPlaceHolderText;
+
+    public Color guessPlaceHolderDefaultColor;
+
+    void OnEnable()
+    {
+        words = GetWords();
+        index = -1;
+
+        showUI.SetActive(true);
+        ShowNext();
+    }
+
+    string[] GetWords()
+    {
+        var manager = GameManager.instance;
+        var list = manager.allWords.ToList();
+        var words = new string[manager.amount];
+
+        for(int i = 0; i < manager.amount; i++)
+        {
+            int j = Random.Range(0, list.Count);
+
+            words[i] = list[j];
+            list.RemoveAt(j);
+        }
+
+        return words;
+    }
+
+    public void ShowNext()
+    {
+        if(index < words.Length - 1)
+        {
+            index++;
+            showText.text = words[index];
+        }
+        else
+        {
+            index = 0;
+            StartGuessing();
+        }
+    }
+
+    public void ConfirmGuessing()
+    {
+        if(guessField.text == words[index])
+        {
+            StartCoroutine(CorrectlyGuessedRoutine());
+        }
+        else
+        {
+            guessField.text = string.Empty;
+            guessPlaceHolderText.text = "Wrong!";
+            guessPlaceHolderText.color = Color.red;
+        }
+    }
+
+    IEnumerator CorrectlyGuessedRoutine()
+    {
+        confirmGuessButton.enabled = false;
+        guessField.text = string.Empty;
+        guessPlaceHolderText.text = "Correct!";
+        guessPlaceHolderText.color = Color.green;
+
+        yield return new WaitForSeconds(1);
+
+        index++;
+
+        if(index < words.Length)
+        {
+            guessPlaceHolderText.text = "Enter Text...";
+            guessPlaceHolderText.color = guessPlaceHolderDefaultColor;
+            confirmGuessButton.enabled = true;
+        }
+        else
+        {
+            guessUI.SetActive(false);
+            congratulationsUI.SetActive(true);
+        }
+    }
+
+    void StartGuessing()
+    {
+        showUI.SetActive(false);
+        guessUI.SetActive(true);
+        confirmGuessButton.enabled = true;
+    }
+}
